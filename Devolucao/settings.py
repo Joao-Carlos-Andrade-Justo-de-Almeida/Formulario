571e05bb81eb61e6bb9ffe0d2ae3a81090f9eb44
João Carlos <justodeandrade@gmail.com>
2025-02-09 12:25:18 -0300

    Mudando o banco do sqlite3 para MySql

--- Devolucao/settings.py
+++ Devolucao/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'Devolucao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'formulario',
+        'USER': 'user',
+        'PASSWORD': 'form1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
